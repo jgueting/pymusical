815f77b41730b2357c5939ae1d586de410283580
Johannes Güting <jgueting@googlemail.com>
2021-02-06 18:42:18 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymusical", # Replace with your own username
+    name="pymusical",
     version="0.1.1",
     author="Johannes Güting",
     author_email="jgueting@googlemail.com",
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jgueting/pymusical",
-    packages=setuptools.find_packages('pyparsing'),
+    packages=setuptools.find_packages(),
+    install_requires=['pyparsing'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

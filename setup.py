bdbc8930521e741071c52239ed57ecb565691882
Johannes Güting <jgueting@googlemail.com>
2021-02-27 19:08:01 +0100

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pymusical",
-    version="0.2.0",
+    version="0.2.1",
     author="Johannes Güting",
     author_email="jgueting@googlemail.com",
     description="A small converter for musical and physical values",
